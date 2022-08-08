e70ee804ee35a4e246a3a528441c371bfdbea5b1
Varzil <varzilthakkarvvv@gmail.com>
2022-08-09 00:09:27 +0530

    adding Pointers.c

---
+++ C/Cpp Essentials-Basics/Pointers.c
@@ -0,0 +1,20 @@
+//
+// Created by Varzil Thakkar on 08/08/22.
+//
+#include "stdio.h"
+#include "stdlib.h"
+struct Rect{
+    int length;
+    int breath;
+};
+int main(){
+    struct Rect r={10,5};
+    printf("%d %d \n",r.length,r.breath);
+
+    struct Rect *ptr=&r; //This is used to make a pointer that points on a struct in stack memory
+
+    printf("%d %d\n",ptr->length,ptr->breath);
+    // Here if you want call the pointer then use ->
+
+    return 0;
+}
