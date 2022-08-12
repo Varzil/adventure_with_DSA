25333e9727ae16b568504b65ece1919fb5533944
Varzil <varzilthakkarvvv@gmail.com>
2022-08-12 10:00:49 +0530

    adding CallByAddress.c

---
+++ C/C Essentials-Basics/CallByAddress.c
@@ -0,0 +1,19 @@
+//
+// Created by Varzil Thakkar on 12/08/22.
+//
+#include "stdio.h"
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(void){
+    int x,y;
+    printf("Enter values of x and y \n");
+    scanf("%d",&x);
+    scanf("%d",&y);
+    printf("Value of x=%d and y=%d \n",x,y);
+    swap(&x,&y);
+    printf("Value of x=%d and y=%d \n",x,y); //Actual parameters get changed
+}
