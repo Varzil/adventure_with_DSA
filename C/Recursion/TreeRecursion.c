c345ea4d29bef652c55f7a7674dfc98692a47673
Varzil <varzilthakkarvvv@gmail.com>
2022-08-25 20:09:38 +0530

    Adding Recursion_Files 🔂

--- C/Recursion/TreeRecursion.c
+++ C/Recursion/TreeRecursion.c
@@ -1,3 +1,17 @@
 //
 // Created by Varzil Thakkar on 25/08/22.
 //
+#include "stdio.h"
+void fun(int n){
+    if(n>0){
+        printf("%d\t",n);
+        fun(n-1);
+        printf("\n%d",n);
+        fun(n-1);
+        // More than one time recursive call
+        // is TreeRecursion
+    }
+}
+int main(void ){
+    fun(4);
+}
