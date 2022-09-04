ff216a67751f02a8bb183327a07c7130883741d3
Varzil <varzilthakkarvvv@gmail.com>
2022-09-04 10:32:22 +0530

    Adding TaylorSeries.c 🔂

--- C/Recursion/TaylorSeries.c
+++ C/Recursion/TaylorSeries.c
@@ -2,12 +2,19 @@
 // Created by Varzil Thakkar on 01/09/22.
 //
 // tay(x)=1+x/1!+x^2/2!+x^3/3!+x^4/4!+.....x^n/n!
-
 #include "stdio.h"
-int taylor(int n){
-    if (n==0)
+double e(int x, int n)
+{
+    static double p=1,f=1;
+    double r;
+    if(n==0)
         return 1;
-    return taylor(n-1);
+    r=e(x,n-1);
+    p=p*x;
+    f=f*n;
+    return r+p/f;
 }
-int main(void){
+int main() {
+    printf("%lf \n", e(4, 15));
+    return 0;
 }
