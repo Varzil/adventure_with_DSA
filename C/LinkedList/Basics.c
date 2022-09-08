4ea301f9a5c8371e8a0c1fc91db617e7acb479af
Varzil <varzilthakkarvvv@gmail.com>
2022-09-08 22:48:04 +0530

    Adding LinkedList Files 🔂

--- C/LinkedList/Basics.c
+++ C/LinkedList/Basics.c
@@ -1,11 +1,14 @@
 //
 // Created by Varzil Thakkar on 08/09/22.
 //
+#include "stdlib.h"
 struct node{
     int val;
     struct node *next;
 };
 int main(void){
-    struct node headNode;
-    headNode.val=10;
+    struct node *headNode;
+    headNode=(struct node *)malloc(sizeof(struct node));
+    headNode->val=10;
+    headNode->next=NULL;
 }
