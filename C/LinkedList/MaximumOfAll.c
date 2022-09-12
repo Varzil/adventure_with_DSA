4b3cc8417338390901e37bc9af0cd91761bdee93
Varzil <varzilthakkarvvv@gmail.com>
2022-09-13 00:12:58 +0530

    Adding LinkedList Files 🔂

--- C/LinkedList/MaximumOfAll.c
+++ C/LinkedList/MaximumOfAll.c
@@ -3,31 +3,51 @@
 //
 #include "stdlib.h"
 #include "stdio.h"
+
 struct node{
     int data;
     struct node *next;
 }*first;
 
-void create(int a[],int n);
+void create(const int a[],int n);
+int max(struct node *p);
+void display(struct node *p);
 
 int main(void){
     int ae={0,2,4,6,2,4,5};
     create(ae,7);
-
+    display(first);
+    printf("Max is %d", max(first));
 }
 
-void create(int a[],int n){
+void create(const int arr[],int n){
     struct node *t,*last;
-    first->data=a[0];
+    first=(struct node *)malloc(sizeof(struct node));
+    first->data=arr[0];
     first->next=NULL;
     last=first;
-    for (int i = 1; i < n; i++) {
-        t->data=a[i];
+    for (int j = 1; j < n; j++) {
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=arr[j];
         t->next=NULL;
         last->next=t;
         last=t;
     }
 }
+void display(struct node *p){
+    while (p!=NULL){
+        printf("%d ->",p->data);
+        p=p->next;
+    }
+    printf("NULL");
+}
 int max(struct node *p){
-
+    int max=INT32_MIN;
+    while(p){
+        if(p->data>max){
+            max=p->data;
+        }
+        p=p->next;
+    }
+    return max;
 }
