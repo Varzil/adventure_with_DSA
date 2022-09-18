8efd440abe9c442d36c04d062ac8421a215c51aa
Varzil <varzilthakkarvvv@gmail.com>
2022-09-18 23:17:28 +0530

    Adding Stack Program 🔂

--- C/Stacks/Basic.c
+++ C/Stacks/Basic.c
@@ -12,5 +12,18 @@ void create(struct Stack *st){
     printf("Enter Size");
     scanf("%d",&st->size);
     st->top=-1;
-    st->s=(int *)malloc(st->size*sizeof(int));
+    st->s=(int *)malloc(st->size*sizeof(int)); // Creating Array of size
 };
+int isEmpty(struct Stack *st){
+    if((st->top=-1))
+        return 1;
+    return 0;
+}
+int isFull(struct Stack *st){
+    if((st->top= st->size-1))
+        return 1;
+    return 0;
+}
+int main(void ){
+
+}
