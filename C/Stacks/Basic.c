6867d9fc4dfb29d035a72985c3ab89ac8260c40f
Varzil <varzilthakkarvvv@gmail.com>
2022-09-18 23:33:06 +0530

    Adding Stack Program 🔂

--- C/Stacks/Basic.c
+++ C/Stacks/Basic.c
@@ -9,21 +9,47 @@ struct Stack{
     int *s;
 };
 void create(struct Stack *st){
-    printf("Enter Size");
+    printf("Enter Size : ");
     scanf("%d",&st->size);
     st->top=-1;
-    st->s=(int *)malloc(st->size*sizeof(int)); // Creating Array of size
+    st->s=(int *)malloc((st->size)*sizeof(int)); // Creating Array of size
 };
 int isEmpty(struct Stack *st){
-    if((st->top=-1))
+    if((st->top==-1))
         return 1;
     return 0;
 }
-int isFull(struct Stack *st){
-    if((st->top= st->size-1))
-        return 1;
-    return 0;
-}
-int main(void ){
 
+int isFull(struct Stack *st){
+    if((st->top== st->size-1))
+        return 1;
+    return 0;
+}
+
+void push(struct Stack *st,int x){
+    if(isFull(&st)==0){
+        st->top++;
+        st->s[st->top]=x;
+        printf("%d Added SuccessFully\n",st->s[st->top]);
+    } else{
+        printf("Stack Full\n");
+    }
+}
+
+int main(void ){
+    struct Stack *stack ;
+    create(&stack);
+
+//    if(isEmpty(&stack)==1){
+//        printf("\nEmpty");
+//    } else{
+//        printf("\nNot Empty");
+//    }
+//    if(isFull(&stack)==1){
+//        printf("\nFull");
+//    } else{
+//        printf("\nNot Full");
+//    }
+
+    push(&stack,1);
 }
