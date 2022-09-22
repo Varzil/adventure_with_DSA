4a70efa7f772162c99f5f471549813e131ed18fe
Varzil <varzilthakkarvvv@gmail.com>
2022-09-22 23:08:44 +0530

    Adding CircularQueue.c of Queue in C 🔂

---
+++ C/Queue/CircularQueue.c
@@ -0,0 +1,57 @@
+//
+// Created by Varzil Thakkar on 22/09/22.
+//
+#include "stdio.h"
+#include "stdlib.h"
+struct Queue{
+    int size;
+    int front;
+    int rear;
+    int *x;
+};
+void create(struct Queue *q){
+    q->rear=q->front=0;
+    printf("Enter the size\n");
+    int size;
+    scanf("%d",&size);
+    q->size=size;
+    q->x= (int *)malloc(size*sizeof(int));
+}
+void enqueue(struct Queue *q,int x){
+    if((q->rear+1)%q->size==q->front){
+        printf("Queue is full\n");
+    }
+    q->rear=(q->rear+1)%q->size;
+    q->x[q->rear]=x;
+    printf("%d added\n",q->x[q->rear]);
+}
+void delete(struct Queue *q){
+    if(q->front==q->rear){
+        printf("The queue is Empty\n");
+    }
+    q->front=(q->front+1)%q->size;
+    printf("%d deleted\n",q->x[q->front]);
+    q->x[q->front]=NULL;
+}
+void display(struct Queue *q){
+    for (int i = 0; i < q->size; i++) {
+        printf("%d \t",q->x[i]);
+    }
+    printf("\n");
+}
+int main(void){
+    struct Queue *queue;
+    int x;
+    create(&queue);
+    enqueue(&queue,3);
+    delete(&queue);
+    enqueue(&queue,2);
+    display(&queue);
+    enqueue(&queue,1);
+    enqueue(&queue,1);
+    enqueue(&queue,1);
+    enqueue(&queue,1);
+    enqueue(&queue,1);
+    delete(&queue);
+    display(&queue);
+}
